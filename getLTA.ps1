bcee5789c8937a9d6174946666a2cad698233364
raveendrank <hannan.ravi@gmail.com>
2025-01-24 07:00:22 +0000

    script Update getLTA.ps1

--- getLTA.ps1
+++ getLTA.ps1
@@ -2,36 +2,15 @@
     [parameter(Mandatory = $false)]
     [string]$License,
     [parameter(Mandatory = $false)]
-    [string]$RunnerOs 
+    [string]$model 
 )
 
-Write-Output "Testing PowerShell script execution..."
+Write-Output "Lemon Tree script start..."
 
-# Check if parameters are provided
-if ([string]::IsNullOrWhiteSpace($License)) {
-    Write-Output "No License info provided."
-} else {
-    Write-Output "License info: $License"
-}
+Set-ExecutionPolicy RemoteSigned -Scope Process -Force
 
-if ($RunnerOs -eq 'Linux') {
-    Write-Output "Running on Linux"
-} elseif ($RunnerOs -eq 'Windows') {
-    Write-Output "Running on Windows"
-} else {
-    Write-Output "Unsupported OS: $RunnerOs"
-}
+& .\LTA\LemonTree.Automation.exe consistencycheck --model $model --license $License
 
-# Create a test output file to verify script execution
-$testOutput = "test_output.txt"
-"Test completed successfully on $(Get-Date)" | Out-File -FilePath $testOutput
-
-# Check if the file was created
-if (Test-Path $testOutput) {
-    Write-Output "Test output file created successfully: $testOutput"
-} else {
-    Write-Output "Failed to create the output file."
-}
-
+Write-Output "Lemon Tree script end..."
 # Exit with success code
 exit 0
